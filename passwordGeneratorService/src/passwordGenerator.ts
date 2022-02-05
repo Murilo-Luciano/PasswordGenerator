47e73fa4555052972417e27743255f7ad3c35c34
Murilo-Luciano <murilo.luciano123@gmail.com>
2022-02-05 15:28:38 -0400

    refactor on passwordGenerator

--- passwordGeneratorService/src/passwordGenerator.ts
+++ passwordGeneratorService/src/passwordGenerator.ts
@@ -20,22 +20,24 @@ const initialOptions = {
   hasSymbols: true,
 };
 
-export function passwordGenerator(
+function passwordGenerator(
   length: number,
   options: PasswordOptionProps = initialOptions
-) {
-  const possibility = handleOptions(range(33, 127), options);
+): string {
+  const charCodePossibilities = handleOptions(range(33, 126), options);
 
   function charGenerator(): string {
     const char = String.fromCharCode(
-      possibility[Math.floor(Math.random() * (max - min) + min)]!
+      charCodePossibilities[getRandomOnRange(max, min)]
     );
 
     return char === "\x00" ? charGenerator() : char;
   }
 
   const min = Math.ceil(0);
-  const max = Math.floor(possibility[possibility.length - 1]!);
+  const max = Math.floor(
+    charCodePossibilities[charCodePossibilities.length - 1]
+  );
   const password = [];
   for (let i = 0; i < length; i++) {
     password.push(charGenerator());
@@ -43,55 +45,46 @@ export function passwordGenerator(
   return password.join("");
 }
 
-function range(start: number, stop?: number, step?: number) {
-  if (typeof stop == "undefined") {
-    // one param defined
-    stop = start;
-    start = 0;
-  }
-
-  if (typeof step == "undefined") {
-    step = 1;
-  }
-
-  if ((step > 0 && start >= stop) || (step < 0 && start <= stop)) {
-    return [];
-  }
-
-  var result = [];
-  for (var i = start; step > 0 ? i < stop : i > stop; i += step) {
-    result.push(i);
-  }
-
-  return result;
-}
-
-function removePossibilities(possibility: number[], removeOptions: number[]) {
-  return possibility.filter((element) => removeOptions.indexOf(element) == -1);
-}
-
 function handleOptions(possibility: number[], options: PasswordOptionProps) {
-  const symbolRanges = [
+  const symbolsCharRanges = [
     range(33, 48),
     range(58, 65),
     range(91, 97),
-    range(123, 127),
+    range(123, 126),
   ];
 
-  if (options.hasNumber === false) {
-    possibility = removePossibilities(possibility, range(48, 58));
+  if (!options.hasNumber) {
+    possibility = removePossibilities(possibility, range(48, 57));
   }
-  if (options.hasLowercase === false) {
-    possibility = removePossibilities(possibility, range(97, 123));
+  if (!options.hasLowercase) {
+    possibility = removePossibilities(possibility, range(97, 122));
   }
-  if (options.hasUppercase === false) {
-    possibility = removePossibilities(possibility, range(65, 91));
+  if (!options.hasUppercase) {
+    possibility = removePossibilities(possibility, range(65, 90));
   }
-  if (options.hasSymbols === false) {
-    symbolRanges.forEach(
+  if (!options.hasSymbols) {
+    symbolsCharRanges.forEach(
       (range) => (possibility = removePossibilities(possibility, range))
     );
   }
 
   return possibility;
 }
+
+function range(start: number, stop: number) {
+  const result = Array.from({ length: stop - start + 1 });
+
+  return result.map((x, i) => {
+    return (x = start + i);
+  });
+}
+
+function removePossibilities(possibility: number[], removeOptions: number[]) {
+  return possibility.filter((element) => removeOptions.indexOf(element) == -1);
+}
+
+function getRandomOnRange(max: number, min: number) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+export default passwordGenerator;
