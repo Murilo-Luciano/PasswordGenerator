e679f925ca42bd04f9b6081fd6a57c443b9a3bdb
Murilo-Luciano <murilo.luciano123@gmail.com>
2022-05-07 10:56:07 -0400

    added theme

--- application/src/App.tsx
+++ application/src/App.tsx
@@ -1,11 +1,9 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { PasswordGeneratorClient } from "./grpc/proto/password_grpc_web_pb";
-import { GenPasswordRequest, Options } from "./grpc/proto/password_pb";
 import { client, clientMessage, options } from "./grpc/client";
-import { Button, createStyles, makeStyles, Paper } from "@mui/material";
+import { Button, Box, Paper } from "@mui/material";
 import OptionsContainer from "./OptionsContainer";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 function App() {
   const [password, setPassword] = useState("");
@@ -32,19 +30,47 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <Paper elevation={4} style={styles.paper}>
-        <h2>Password: {password}</h2>
-        <OptionsContainer handleSizeChange={handlePasswordSizeChange} />
-        <Button onClick={onClick}>generate password</Button>
-      </Paper>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Box style={styles.container}>
+        <Paper elevation={5} style={styles.paper}>
+          <h2>Password: {password}</h2>
+          <OptionsContainer handleSizeChange={handlePasswordSizeChange} />
+          <Button variant="contained" onClick={onClick}>
+            generate password
+          </Button>
+        </Paper>
+      </Box>
+    </ThemeProvider>
   );
 }
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#5bccf6",
+    },
+    secondary: {
+      main: "#030e12",
+    },
+    background: {
+      default: "#fef9f8",
+      paper: "#fcde67",
+    },
+  },
+});
 
 const styles: { [key: string]: React.CSSProperties } = {
   paper: {
     margin: 150,
+    padding: 50,
+    display: "flex",
+    flexDirection: "column",
+  },
+  container: {
+    position: "absolute",
+    height: "100%",
+    width: "100%",
+    backgroundColor: theme.palette.background.default,
   },
 };
 
